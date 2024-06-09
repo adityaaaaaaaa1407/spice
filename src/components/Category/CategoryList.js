0029a90a0fb719aa211a70c757e525e2bb67642c
Aditya Sagavekar <adityasagavekar14@gmail.com>
2024-06-09 17:49:23 +0530

    done with errors

--- src/components/Category/CategoryList.js
+++ src/components/Category/CategoryList.js
@@ -14,7 +14,7 @@ const CategoryList = ({categories}) => {
 
                 return (
                   <Link to = {`/meal/category/${title}`} className = "category-itm align-center justify-center" key = {id}>
-                    <div className='category-itm-img flex align-center justify-center gap-4 mb-4'>
+                    <div className='relative flex align-center justify-center gap-4 mb-4'>
                       <div className =" mx-auto p-4"><img src = {thumbnail} alt = {title} className="w-full h-auto max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl gap-4 mb-4"/></div>
                       
                       <div className='category-itm-title bg-orange'>
